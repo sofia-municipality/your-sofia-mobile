90567127263be4125b1ed4021acfe23cbb6edc71
quantum-grit <91589884+quantum-grit@users.noreply.github.com>
2025-12-12 21:35:55 +0200

    now added env placeholder in app.config.js

---
+++ app.config.js
@@ -0,0 +1,11 @@
+module.exports = ({config}) => ({
+  ...config,
+  android: {
+    ...config.android,
+    config: {
+      googleMaps: {
+        apiKey: process.env.GOOGLE_MAPS_ANDROID_API_KEY,
+      },
+    },
+  },
+});
